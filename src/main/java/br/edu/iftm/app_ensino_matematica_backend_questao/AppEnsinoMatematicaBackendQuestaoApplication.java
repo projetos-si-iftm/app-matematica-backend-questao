96104cba09d157d214ab67c2f2d9da2c18a4a279
fromanoel <fernanda.romanoel@estudante.iftm.edu.br>
2025-02-06 16:47:02 -0300

    config

---
+++ src/main/java/br/edu/iftm/app_ensino_matematica_backend_questao/AppEnsinoMatematicaBackendQuestaoApplication.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.app_ensino_matematica_backend_questao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEnsinoMatematicaBackendQuestaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppEnsinoMatematicaBackendQuestaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/iftm/app_ensino_matematica_backend_questao/AppEnsinoMatematicaBackendQuestaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.app_ensino_matematica_backend_questao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppEnsinoMatematicaBackendQuestaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
