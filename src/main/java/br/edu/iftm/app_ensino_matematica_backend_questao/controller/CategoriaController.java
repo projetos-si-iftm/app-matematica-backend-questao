e54660f586cdb78c6d6be4a18e67eae85098594d
fromanoel <fernanda.romanoel@estudante.iftm.edu.br>
2025-02-18 16:29:12 -0300

    refactor: adicao dos status response

--- src/main/java/br/edu/iftm/app_ensino_matematica_backend_questao/controller/CategoriaController.java
+++ src/main/java/br/edu/iftm/app_ensino_matematica_backend_questao/controller/CategoriaController.java
@@ -3,6 +3,7 @@ package br.edu.iftm.app_ensino_matematica_backend_questao.controller;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.iftm.app_ensino_matematica_backend_questao.model.Categoria;
@@ -24,26 +26,32 @@ public class CategoriaController {
     private final CategoriaService categoriaService;
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<CategoriaDTO> getAll(){
         return categoriaService.getAll();
     }
 
+
     @GetMapping("/{id_categoria}")
+    @ResponseStatus(HttpStatus.OK)
     public Categoria findById(@PathVariable UUID id_categoria){
         return categoriaService.findById(id_categoria);
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public CategoriaDTO save(@RequestBody CategoriaDTO categoriaDTO){
         return categoriaService.save(categoriaDTO);
     }
 
     @DeleteMapping("/{id_categoria}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public CategoriaDTO delete(@PathVariable UUID id_categoria){
         return categoriaService.delete(id_categoria);
     }
 
     @PatchMapping("/{id_categoria}")
+    @ResponseStatus(HttpStatus.OK)
     public CategoriaDTO update(@PathVariable UUID id_categoria, @RequestBody CategoriaDTO categoriaDTO){
         return categoriaService.update(id_categoria, categoriaDTO);
     }
--- src/main/java/br/edu/iftm/app_ensino_matematica_backend_questao/controller/QuestaoController.java
+++ src/main/java/br/edu/iftm/app_ensino_matematica_backend_questao/controller/QuestaoController.java
@@ -3,12 +3,14 @@ package br.edu.iftm.app_ensino_matematica_backend_questao.controller;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.iftm.app_ensino_matematica_backend_questao.model.DTO.QuestaoDTO;
@@ -22,31 +24,37 @@ public class QuestaoController {
     private final QuestaoService questaoService;
     
     @PostMapping
+    @ResponseStatus(HttpStatus.OK)
     public QuestaoDTO save( @RequestBody QuestaoDTO questaoDTO){
         return questaoService.save(questaoDTO);
     }
 
     @GetMapping("/{id_questao}")
+    @ResponseStatus(HttpStatus.OK)
     public QuestaoDTO findById(@PathVariable UUID id_questao){
         return questaoService.findById(id_questao);
     }
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<QuestaoDTO> getAll(){
         return questaoService.getAll();
     }
 
     @GetMapping("/category/{id_categoria}")
+    @ResponseStatus(HttpStatus.OK)
     public List<QuestaoDTO> findByCategoria(@PathVariable UUID id_categoria) {
         return questaoService.findByCategoria(id_categoria);
     }
 
     @GetMapping("/category/dificulty")
+    @ResponseStatus(HttpStatus.OK)
     public List<QuestaoDTO> findByCategoriaAndDificuldade(@RequestParam UUID id_categoria, @RequestParam int dificuldade) {
         return questaoService.findByCategoriaAndDificuldade(id_categoria, dificuldade);
     }
 
     @GetMapping("/category/dificulty/search")
+    @ResponseStatus(HttpStatus.OK)
     public List<QuestaoDTO> findRandomByCategoriaAndDificuldade(@RequestParam UUID id_categoria, @RequestParam int dificuldade) {
         return questaoService.findRandomByCategoriaAndDificuldade(id_categoria, dificuldade);
     }
